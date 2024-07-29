920d5d0089032b709736143469a35e42e0396be6
Kariuki_Ken <irungukenneth433@gmail.com>
2024-07-29 16:13:21 +0300

    cha

--- cados_api/base/views.py
+++ cados_api/base/views.py
@@ -3,34 +3,6 @@ from django.http import JsonResponse
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
-from.models import Advocate
-from .serializers import AdvocateSerializer
-
-# Create your views here.
-
-
-@api_view(['GET'])
-def endpoints(request):
-    data = ['/advocates', 'advocates/:username']
-    return JsonResponse(data, safe=False)
-
-@api_view(['GET'])
-def advocate_list(request):
-    #data = ['Dennis', 'Tadas', 'Max']
-    advocates = Advocate.objects.all()
-    serializer = AdvocateSerializer(advocates, many=True)
-    return Response(advocates.data)
-
-@api_view(['GET'])
-def advocate_detail(request, username):
-    data = username
-    return JsonResponse(data, safe=False)
-
-from django.shortcuts import render
-from django.http import JsonResponse
-from rest_framework.decorators import api_view
-from rest_framework.response import Response
-
 from .models import Advocate
 from .serializers import AdvocateSerializer
 
