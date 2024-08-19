37ffbd314f387464c41c12da9e613d9d066cda91
Kariuki_Ken <irungukenneth433@gmail.com>
2024-08-19 16:34:44 +0300

    New changes

--- cados_api/base/urls.py
+++ cados_api/base/urls.py
@@ -4,5 +4,5 @@ from .import views
 urlpatterns = [
     path('', views.endpoints),
     path('advocates/', views.advocate_list),
-    path('advocates/<str:username>/', views.advocate_detail)
+    path('advocates/<str:username>/', views.advocate_detail),
 ]
